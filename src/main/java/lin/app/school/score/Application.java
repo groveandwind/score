78ce9ad9776e7eea4bffe39e24efed2e5f4e79d3
lin <lin@pc.com>
2018-06-14 23:04:14 +0800

    项目结构

--- src/main/java/lin/app/school/score/Application.java
+++ src/main/java/lin/app/school/score/Application.java
@@ -3,9 +3,15 @@ package lin.app.school.score;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
+import tk.mybatis.spring.annotation.MapperScan;
 
 @SpringBootApplication
 @EnableAutoConfiguration
+@EnableTransactionManagement
+@EnableCaching
+@MapperScan(basePackages = "lin.app.*.dao")
 public class Application {
 
     public static void main(String[] args) {
